3adec5bb2560713724cf4fcf544c06a4e05720da
babududekula <babu1523y@gmail.com>
2024-09-11 01:45:07 +0530

    changes for eureka

--- 40-Spring-Boot-Microservices-Admin-Server-For-Eureka/src/main/java/com/pack/Application.java
+++ 40-Spring-Boot-Microservices-Admin-Server-For-Eureka/src/main/java/com/pack/Application.java
@@ -11,6 +11,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("Welcome To Eureka Server...");
 	}
 
 }
